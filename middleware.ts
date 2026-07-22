2f4456644c9d07bfa6b78c7f99680f46c54ea7fc
Makirow-Makia Yengue <godwillmakiayengue@gmail.com>
2026-07-22 14:35:02 +0100

    Enhance route protection with auth redirection
    Updated middleware to protect routes and redirect unauthenticated users.

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,16 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 // Define public routes using createRouteMatcher
 const isPublicRoute = createRouteMatcher([
-  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
   '/api/webhooks/clerk',
   '/api/webhooks/stripe',
 ]);
 
 export default clerkMiddleware((auth, req) => {
   // Protect all routes except public ones
-  if (!isPublicRoute(req)) {
-    auth();
+ if (!isPublicRoute(req)) {
+    await auth.protect() // Redirects unauthenticated users to /sign-in
   }
 });
 
